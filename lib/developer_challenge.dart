1a68fd2a551eb56dcfaa5e0d9223ec1cedd7180a
Khang Nguyen <dsy1090@wmich.edu>
2025-05-10 04:25:30 -0500

    make developer challenge page

---
+++ lib/developer_challenge.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeveloperChallengePage extends StatefulWidget {
+  const DeveloperChallengePage({super.key});
+
+  @override
+  State<DeveloperChallengePage> createState() => _DeveloperChallengeState();
+}
+
+class _DeveloperChallengeState extends State<DeveloperChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
