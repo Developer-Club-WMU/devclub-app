6fefe4f8963c491c9a41197a81f2ae47dbc3d606
Khang Nguyen <dsy1090@wmich.edu>
2025-05-10 04:25:22 -0500

    move scaffold to main

--- lib/home.dart
+++ lib/home.dart
@@ -11,21 +11,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   
-  // state properties - usage example: _counter
-    int currentPageIndex = 0;
-
   @override
-  Widget build(BuildContext context) {
-    
-    final theme = Theme.of(context); // example usage: "theme.colorScheme.primary" (https://docs.flutter.dev/cookbook/design/themes)
-    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.colorScheme.primary,
-
-        title: Text('Upcoming Event!'),
-      ),
-      body: Center(
+  Widget build(BuildContext context) {    
+    return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -34,31 +22,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: theme.colorScheme.primary,
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.event_outlined),
-            selectedIcon: Icon(Icons.event),
-            label: 'Events',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.ballot_outlined),
-            selectedIcon: Icon(Icons.ballot),
-            label: 'Project Hub',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.emoji_events_outlined),
-            selectedIcon: Icon(Icons.emoji_events),
-            label: 'School',
-          ),
-        ],
-    ));
+      );
   }
 }
