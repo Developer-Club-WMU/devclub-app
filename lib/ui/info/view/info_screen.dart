afb1f123f11158c3e98395ba0abdc5330f780acc
Khang Nguyen <dsy1090@wmich.edu>
2025-06-09 23:49:25 -0400

    new: added temporary info page masonry view

--- lib/ui/info/view/info_screen.dart
+++ lib/ui/info/view/info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class InfoScreen extends StatefulWidget {
   const InfoScreen({super.key});
@@ -8,27 +9,41 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
+  
+  // TODO: Temporary list of colors to be used in the grid, remove when actual data is available
+  final List<Color> colors = [
+    Colors.brown, Colors.yellow, Colors.brown, Colors.yellow,
+    Colors.brown, Colors.yellow, Colors.brown, Colors.yellow,
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'Info Page',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+      child: CustomScrollView(
+        slivers: <Widget>[
+
+          // SliverAppBar
+          SliverAppBar(
+            title: const Text('Info'),
+            floating: false,
+            pinned: false,
+            snap: false,
+            expandedHeight: 100.0,
           ),
 
-          const Text(
-            'This is the info page.',
-          ),
-
-          SizedBox(
-            height: 10,
-          ),
+          SliverMasonryGrid.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            childCount: colors.length,
+            itemBuilder: (context, index) {
+                return Container(
+                  height: 100.0 + (index % 4) * 30,
+                  color: colors[index],
+                  child: Center(child: Text("Tile $index")),
+                );
+            },
+          )
         ],
       ),
     );
