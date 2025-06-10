200c4be013b2d34a17d628cf03b39e9555fa520f
Khang Nguyen <dsy1090@wmich.edu>
2025-06-10 13:41:14 -0400

    new: create the model for members

---
+++ lib/data/models/member_model.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Member {
+  
+  // Constructor
+  const Member ({
+    required this.id,
+    this.email,
+    this.emailVerified = false,
+    this.displayName,
+  });
+
+  // Properties
+  final String id;
+  final String? email;
+  final bool emailVerified;
+  final String? displayName;
+
+  // Methods
+  /// This function map the Firebase User to our User dart model
+  static Member? fromUser(User? user) {
+    if (user == null) {
+      return null;
+    }
+    return Member(
+      id: user.uid,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      displayName: user.displayName,
+    );
+  }
+}
