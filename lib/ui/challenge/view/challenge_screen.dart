2e97261cac85d4db42da5c75677c55b88347698a
Khang Nguyen <dsy1090@wmich.edu>
2025-06-09 22:46:20 -0400

    new: populate challenge screen with placeholders containers

--- lib/ui/challenge/view/challenge_screen.dart
+++ lib/ui/challenge/view/challenge_screen.dart
@@ -14,17 +14,61 @@ class _ChallengesScreenState extends State<ChallengesScreen> {
     final theme = Theme.of(context);
 
     return PageView.builder(
-      itemCount: 3,
+      itemCount: 7,
       physics: const ClampingScrollPhysics(),
       controller: PageController(viewportFraction: 1),
       itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.symmetric(horizontal: 5),
-          color: theme.colorScheme.primary,
-          child: Center(child: Text("Page $index")),
-        );
-    },
-  );
-
+        switch(index) {
+          case 0:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("W1 View")),
+            );
+          case 1:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("Web Community")),
+            );
+          case 2:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("App Community")),
+            );
+            case 3:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("AI Community")),
+            );
+            case 4:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("Game Community")),
+            );
+            case 5:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("System Community")),
+            );
+            case 6:
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              color: theme.colorScheme.primary,
+              child: Center(child: Text("Hackathon Community")),
+            );
+            default:
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("Default View")),
+              );
+        }
+      },
+    );
   }
 }
