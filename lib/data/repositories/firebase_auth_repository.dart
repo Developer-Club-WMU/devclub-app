e406f62c52c46ebed90d86c1df137399bc2384ed
Khang Nguyen <dsy1090@wmich.edu>
2025-06-10 13:42:17 -0400

    new: create a repository for authentication feature

---
+++ lib/data/repositories/firebase_auth_repository.dart
@@ -0,0 +1,47 @@
+import 'package:devclub_app/data/models/member_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthenticationRepository{
+  // Constructor
+  FirebaseAuthenticationRepository(this._firebaseAuth);
+  
+  final FirebaseAuth _firebaseAuth;
+
+  /// This function gets the current user
+  Member? get currentUser => _convertUser(_firebaseAuth.currentUser);
+
+  /// This function takes a firebase user and converts it to our Member model if the passed in user is not null
+  Member? _convertUser(User? user) => user == null ? null : Member.fromUser(user);
+
+  /// This function allows the app to listen to the user state change
+  Stream<Member?> authStateChanges() {
+    return _firebaseAuth.authStateChanges().map(_convertUser);
+  }
+
+  /// This function allows the member to sign in with email and password
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+    );
+  }
+
+  /// This function allows the member to sign out
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) {
+      return _firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+      );
+  }
+
+  /// This function allows the member to sign out
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
+}
