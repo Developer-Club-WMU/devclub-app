754bf7a2f3fb0be5ad2bcea98afdb39d1b06117b
Khang Nguyen <dsy1090@wmich.edu>
2025-06-10 02:46:09 -0400

    update: transparent app bar for challenge screen

--- lib/ui/challenge/view/challenge_screen.dart
+++ lib/ui/challenge/view/challenge_screen.dart
@@ -13,62 +13,62 @@ class _ChallengesScreenState extends State<ChallengesScreen> {
 
     final theme = Theme.of(context);
 
-    return PageView.builder(
-      itemCount: 7,
-      physics: const ClampingScrollPhysics(),
-      controller: PageController(viewportFraction: 1),
-      itemBuilder: (context, index) {
-        switch(index) {
-          case 0:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("W1 View")),
-            );
-          case 1:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("Web Community")),
-            );
-          case 2:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("App Community")),
-            );
-            case 3:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("AI Community")),
-            );
-            case 4:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("Game Community")),
-            );
-            case 5:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("System Community")),
-            );
-            case 6:
-            return Container(
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              color: theme.colorScheme.primary,
-              child: Center(child: Text("Hackathon Community")),
-            );
-            default:
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text("Challenges"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: PageView.builder(
+        itemCount: 7,
+        physics: const ClampingScrollPhysics(),
+        controller: PageController(viewportFraction: 1.0),
+        itemBuilder: (context, index) {
+          switch(index) {
+            case 0:
               return Container(
-                margin: EdgeInsets.symmetric(horizontal: 5),
                 color: theme.colorScheme.primary,
-                child: Center(child: Text("Default View")),
+                child: Center(child: Text("W1 View")),
               );
-        }
-      },
+            case 1:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("Web Community")),
+              );
+            case 2:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("App Community")),
+              );
+              case 3:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("AI Community")),
+              );
+              case 4:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("Game Community")),
+              );
+              case 5:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("System Community")),
+              );
+              case 6:
+              return Container(
+                color: theme.colorScheme.primary,
+                child: Center(child: Text("Hackathon Community")),
+              );
+              default:
+                return Container(
+                  color: theme.colorScheme.primary,
+                  child: Center(child: Text("Default View")),
+                );
+          }
+        },
+      ),
     );
   }
 }
