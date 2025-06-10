338bd991c2cf1bfc1b8f5b74ce83979ebcbbd002
Khang Nguyen <dsy1090@wmich.edu>
2025-06-10 02:54:47 -0400

    update: comments and formatting update

--- lib/ui/challenge/view/challenge_screen.dart
+++ lib/ui/challenge/view/challenge_screen.dart
@@ -14,58 +14,72 @@ class _ChallengesScreenState extends State<ChallengesScreen> {
     final theme = Theme.of(context);
 
     return Scaffold(
+
+      // Transparent AppBar with extended body behind it
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text("Challenges"),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
+
+      // Use PageView to create a swipeable view for different challenges
       body: PageView.builder(
         itemCount: 7,
         physics: const ClampingScrollPhysics(),
         controller: PageController(viewportFraction: 1.0),
         itemBuilder: (context, index) {
           switch(index) {
+
+            // TODO: Replace these containers with actual challenge views
             case 0:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("W1 View")),
               );
+
             case 1:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("Web Community")),
               );
+
             case 2:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("App Community")),
               );
+
               case 3:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("AI Community")),
               );
+
               case 4:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("Game Community")),
               );
+
               case 5:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("System Community")),
               );
+
               case 6:
               return Container(
                 color: theme.colorScheme.primary,
                 child: Center(child: Text("Hackathon Community")),
               );
+
               default:
                 return Container(
                   color: theme.colorScheme.primary,
                   child: Center(child: Text("Default View")),
                 );
+                
           }
         },
       ),
