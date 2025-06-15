dabefe037994d4394723a5d54a93693e587398c9
Khang Nguyen <dsy1090@wmich.edu>
2025-06-15 07:43:39 -0400

    feature: add a gradient background

--- lib/ui/event/view/events_screen.dart
+++ lib/ui/event/view/events_screen.dart
@@ -20,74 +20,84 @@ class _EventsScreenState extends ConsumerState<EventsScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     final theme = Theme.of(context);
 
-    // Use CustomScrollView to create a scrollable layout with slivers, potentially adding appbar
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
-      slivers: <Widget>[
-        
-        // Upcoming Workshop Section
-        // TODO: Replace with actual workshop data and UI
-        SliverToBoxAdapter(
-          child: Container(
-            width: screenWidth,
-            height: screenHeight,
-            color: theme.colorScheme.primary,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                  Center(child: Text("Upcoming Workshop")),
-
-                  ElevatedButton(
-                    onPressed: () => context.push('/events_list'),
-                    child: const Text('Go to Events List'),
-                  ),
-
-                  // TODO: Design where the sign up button will be and get rid of this button
-                  ElevatedButton(
-                    onPressed: () async {
-                      await ref.read(authControllerProvider.notifier).signOut();
-                    },
-                    child: const Text('Sign Out'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-        // Upcoming Speaker Event Section
-        // TODO: Replace with actual speaker event data and UI
-        SliverToBoxAdapter(
+    // Apply gradient background with colors fetch from FlutterFlow
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 47, 41, 39),
+            Color.fromARGB(255, 71, 60, 56),
+            Color.fromARGB(255, 79, 75, 93),
+          ]
+        ),
+      ),
+      // Use CustomScrollView to create a scrollable layout with slivers, potentially adding appbar
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: <Widget>[
+          
+          // Upcoming Workshop Section
+          // TODO: Replace with actual workshop data and UI
+          SliverToBoxAdapter(
             child: Container(
               width: screenWidth,
               height: screenHeight,
-              color: theme.colorScheme.secondary,
-              child: Center(child: Text("Upcoming Speaker Event")),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                    Center(child: Text("Upcoming Workshop")),
+      
+                    ElevatedButton(
+                      onPressed: () => context.push('/events_list'),
+                      child: const Text('Go to Events List'),
+                    ),
+      
+                    // TODO: Design where the sign up button will be and get rid of this button
+                    ElevatedButton(
+                      onPressed: () async {
+                        await ref.read(authControllerProvider.notifier).signOut();
+                      },
+                      child: const Text('Sign Out'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+      
+          // Upcoming Speaker Event Section
+          // TODO: Replace with actual speaker event data and UI
+          SliverToBoxAdapter(
+              child: Container(
+                width: screenWidth,
+                height: screenHeight,
+                child: Center(child: Text("Upcoming Speaker Event")),
+              ),
+            ),
+      
+          // Upcoming Hackathon Section
+          // TODO: Replace with actual hackathon data and UI
+          SliverToBoxAdapter(
+            child: Container(
+              width: screenWidth,
+              height: screenHeight,
+              child: Center(child: Text("Upcoming Hackathon")),
             ),
           ),
-
-        // Upcoming Hackathon Section
-        // TODO: Replace with actual hackathon data and UI
-        SliverToBoxAdapter(
-          child: Container(
-            width: screenWidth,
-            height: screenHeight,
-            color: theme.colorScheme.primary,
-            child: Center(child: Text("Upcoming Hackathon")),
+      
+          // Upcoming Community Meeting Section
+          // TODO: Replace with actual community meeting data and UI
+          SliverToBoxAdapter(
+            child: Container(
+              width: screenWidth,
+              height: screenHeight,
+              child: Center(child: Text("Upcoming Community Meeting")),
+            ),
           ),
-        ),
-
-        // Upcoming Community Meeting Section
-        // TODO: Replace with actual community meeting data and UI
-        SliverToBoxAdapter(
-          child: Container(
-            width: screenWidth,
-            height: screenHeight,
-            color: theme.colorScheme.secondary,
-            child: Center(child: Text("Upcoming Community Meeting")),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
