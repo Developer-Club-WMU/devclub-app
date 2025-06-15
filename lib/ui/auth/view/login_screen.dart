2d24c50f579a23bd21a02a18d6316fc7fee1b76f
Khang Nguyen <dsy1090@wmich.edu>
2025-06-15 12:27:02 -0400

    fix + update: got rid of loading and update UI

--- lib/ui/auth/view/login_screen.dart
+++ lib/ui/auth/view/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:devclub_app/route/view_model/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 
 // TODO: Update design with FlutterFlow and Implement here
@@ -17,7 +18,9 @@ class LoginScreen extends ConsumerStatefulWidget {
 class _LoginScreenState extends ConsumerState<LoginScreen> {
 
   // State properties
-  BuildContext? _progressIndicatorContext;
+
+  // this store the progress of the loading indicator in side the log in screen state
+  // BuildContext? _progressIndicatorContext;
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
@@ -28,11 +31,11 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     _emailController.dispose();
     _passwordController.dispose();
     
-    // Close loading dialog when closing page
-    if (_progressIndicatorContext != null && _progressIndicatorContext!.mounted) {
-      Navigator.of(_progressIndicatorContext!).pop();
-      _progressIndicatorContext = null;
-    }
+    // // Close loading dialog when closing page
+    // if (_progressIndicatorContext != null && _progressIndicatorContext!.mounted) {
+    //   Navigator.of(_progressIndicatorContext!).pop();
+    //   _progressIndicatorContext = null;
+    // }
 
     super.dispose();
   }
@@ -45,27 +48,27 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       
       // Show circular progress indicator when loading
       if (state.isLoading) {
-        await showDialog(
-          context: context,
-          builder: (ctx) {
-            _progressIndicatorContext = ctx;
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        );
+        // await showDialog(
+        //   context: context,
+        //   builder: (ctx) {
+        //     _progressIndicatorContext = ctx;
+        //     return const Center(
+        //       child: CircularProgressIndicator(),
+        //     );
+        //   },
+        // );
         return;
       }
 
-      // Close circular progress indicator after rebuild to guarantee that the context is still valid
-      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-        if (_progressIndicatorContext != null && _progressIndicatorContext!.mounted) {
+      // // Close circular progress indicator after rebuild to guarantee that the context is still valid
+      // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      //   if (_progressIndicatorContext != null && _progressIndicatorContext!.mounted) {
           
-          Navigator.of(_progressIndicatorContext!).pop();
-          _progressIndicatorContext = null;
+      //     Navigator.of(_progressIndicatorContext!).pop();
+      //     _progressIndicatorContext = null;
         
-        }
-      });
+      //   }
+      // });
 
       // If loading state has error, show a snackbar with the error message
       if (state.hasError) {
@@ -82,130 +85,197 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     final theme = Theme.of(context);
 
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-
-              // TODO: Add custom logo
-
-              // Welcome Text
-              Text(
-                'Welcome to Developer Club',
-                style: TextStyle(
-
-                  // TODO: Replace with your custom font
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  color: theme.colorScheme.primary,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 47, 41, 39),
+              Color.fromARGB(255, 71, 60, 56),
+              Color.fromARGB(255, 79, 75, 93),
+            ]
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+        
+                // TODO: Add custom logo
+        
+                // Welcome Text
+                Text(
+                  'Welcome to Developer Club',
+                  style: TextStyle(
+        
+                    // TODO: Replace with your custom font
+                    fontFamily: 'Robotto',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 32,
+                    color: theme.colorScheme.onPrimary,
+                  ),
                 ),
-              ),
-
-              SizedBox(height: 6),
-
-              // Login Request
-              Text(
-                'Please Log In to continue',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.normal,
-                  fontSize: 18,
-                  color: theme.colorScheme.onSurface,
+        
+                SizedBox(height: 12),
+        
+                // Login Request
+                Text(
+                  'Please log in to continue!',
+                  style: TextStyle(
+                    fontFamily: 'Robotto',
+                    fontWeight: FontWeight.w100,
+                    fontSize: 16,
+                    color: theme.colorScheme.onSurfaceVariant,
+                  ),
                 ),
-              ),
-
-              SizedBox(height: 26),
-
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email Address',
-                  border: OutlineInputBorder(),
+        
+                SizedBox(height: 12),
+        
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Email!',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Roboto',
+                      color: theme.colorScheme.tertiaryFixedDim,
+                      fontSize: 14,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-
-              SizedBox(height: 16),
-
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+        
+                SizedBox(height: 6),
+        
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Password!',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Roboto',
+                      color: theme.colorScheme.tertiaryFixedDim,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true, // Hide password input, best practice for security
                 ),
-                obscureText: true, // Hide password input, best practice for security
-              ),
+        
+                SizedBox(height: 26),
+        
+                // Login Button
+                SizedBox(
+                  width: double.infinity,
+                  height: 49,
+                  child: SignInButton(
+                    Buttons.Email, 
+                    onPressed: () async {
+                      _logIn();
+                    }
+                  ),
+                ),
 
-              SizedBox(height: 26),
+                SizedBox(height: 8),
 
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                height: 49,
-                child: ElevatedButton(
-                  
-                  onPressed: _logIn,
-
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: theme.colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 49,
+                  child: SignInButton(
+                    Buttons.Google,
+                      
+                    onPressed: () {},       
                     ),
                   ),
 
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                const SizedBox(height: 12),
 
-                ),
-              ),
-              
-              SizedBox(height: 26),
-
-              Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      // TODO: Implement Forgot Password OTP
-                    },
+                Center(
                     child: Text(
-                      'Forgot Password?',
+                      'Forgot Password or Don\'t have an account?',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 14,
                         color: Colors.grey,
                       ),
                     ),
-                  ),
-              ),
+                ),
 
-              SizedBox(height: 10),
+                const SizedBox(height: 12),
+        
+                SizedBox(
+                  width: double.infinity,
+                  height: 49,
+                  child: MaterialButton(
+                    
+                    onPressed: () {},
 
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    context.goNamed(AppRoutes.signUp.name);
-                  },
-                  child: Text(
-                    "Don't have an account? Sign Up",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      color: Colors.grey,
+                    color: theme.colorScheme.inversePrimary,
+
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
                     ),
+
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Sign In With Magic Link',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+        
                   ),
                 ),
-              )
 
-            ],
+                SizedBox(height: 10),
+        
+                SizedBox(
+                  width: double.infinity,
+                  height: 49,
+                  child: MaterialButton(
+                    
+                    onPressed: () {
+                      context.goNamed(AppRoutes.signUp.name);
+                    },
+
+                    color: theme.colorScheme.surface,
+
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Register an account',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+        
+                  ),
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
